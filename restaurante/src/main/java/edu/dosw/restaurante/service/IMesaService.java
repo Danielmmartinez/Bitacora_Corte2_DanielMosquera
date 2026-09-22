32f68d2c08a0c9b17fd5690428446e689498e904
Daniel Mosquera <daniel.mmartinez@mail.escuelaing.edu.co>
2026-09-21 19:55:11 -0500

    Implementacion servicion faltantes

---
+++ restaurante/src/main/java/edu/dosw/restaurante/service/IMesaService.java
@@ -0,0 +1,14 @@
+package edu.dosw.restaurante.service;
+
+import edu.dosw.restaurante.domain.EstadoMesa;
+import edu.dosw.restaurante.domain.Mesa;
+
+import java.util.List;
+
+public interface IMesaService {
+    List<Mesa> obtenerTodas();
+    Mesa obtenerPorId(Long id);
+    Mesa crear(Mesa mesa);
+    Mesa cambiarEstado(Long id, EstadoMesa nuevoEstado);
+    void eliminar(Long id);
+}
